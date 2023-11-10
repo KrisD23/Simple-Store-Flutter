97ebe4498cee8f74ffe1cf4e251b5574039a51b3
Krishan Dwivedi <krishandwive9@gmail.com>
2023-11-10 16:03:06 +0530

    Added logo titil subtitile

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,6 +7,39 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.shopping_bag_outlined,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+            //title
+            Text(
+              "PurePalette",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+            const SizedBox(height: 10),
+
+            //subtitle
+            Text("Unveil Elegance in Every Hue",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary)),
+
+            //button
+          ],
+        ),
+      ),
     );
   }
 }
