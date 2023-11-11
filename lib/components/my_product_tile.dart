8866ca59974c4a22e1c72ef933283e78839a687b
Krishan Dwivedi <krishandwive9@gmail.com>
2023-11-11 12:02:51 +0530

    add to cart button working with dialog

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_ecommerce_app_yt/models/product.dart';
+import 'package:minimal_ecommerce_app_yt/models/shop.dart';
+import 'package:provider/provider.dart';
 
 class MyProductTile extends StatelessWidget {
   final Product product;
   const MyProductTile({required this.product, super.key});
+
+  // add to cart function
+  void addToCart(BuildContext context) {
+    // show a dialog box
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text('Add this item to cart?'),
+        actions: [
+          // cancel button
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancel'),
+          ),
+
+          // yes button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // add to cart
+              context.read<Shop>().addToCart(product);
+            },
+            child: Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,39 +46,62 @@ class MyProductTile extends StatelessWidget {
       padding: const EdgeInsets.all(25),
       width: 300,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // product image
-          AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondary,
-                borderRadius: BorderRadius.circular(10),
+          Column(
+            children: [
+              // product image
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: double.infinity,
+                  padding: EdgeInsets.all(25),
+                  child: Icon(Icons.face_retouching_natural),
+                ),
               ),
-              width: double.infinity,
-              padding: EdgeInsets.all(25),
-              child: Icon(Icons.face_retouching_natural),
-            ),
-          ),
-          const SizedBox(height: 25),
-          // product name
-          Text(
-            product.name,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(height: 10),
+              const SizedBox(height: 25),
+              // product name
+              Text(
+                product.name,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(height: 10),
 
-          // product description
-          Text(
-            product.description,
-            style:
-                TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+              // product description
+              Text(
+                product.description,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+              const SizedBox(height: 25),
+            ],
           ),
-          const SizedBox(height: 25),
 
           // product price + add to cart button
-          Text(product.price.toString()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // product price
+              Text('\$ ' + product.price.toString()),
+
+              // add to cart button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: IconButton(
+                  onPressed: () => addToCart(context),
+                  icon: Icon(Icons.add),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
